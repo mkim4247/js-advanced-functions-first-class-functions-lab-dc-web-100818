e915f89f28cc48d78528e742617000de489e73a1
Michael Kim <m.kim4247@gmail.com>
2020-05-24 14:17:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function(arr){
-  return arr.slice(0, 2);
-}
+const returnFirstTwoDrivers = function (driver) {
+  return driver.slice(0,2);
+};
 
- const returnLastTwoDrivers = function(arr) {
-   return arr.slice(-2);
- }
+const returnLastTwoDrivers =  function (driver) {
+  return driver.slice(driver.length - 2);
+};
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+  ];
 
-const createFareMultiplier = function(num) {
+const createFareMultiplier = function(integer) {
   return function(fare) {
-    return fare * num;
-  }
-}
+    return fare * integer;
+  };
+};
+// for example, this higher order function takes an argument of 4
+// const fareQuadrupler = createFareMultiplier(4);
+// fareQuarupler(12) => 48
 
-const fareDoubler = function(fare) {
-  return createFareMultiplier(2)(fare);
-}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
 
-const fareTripler = function(fare) {
-  return createFareMultiplier(3)(fare);
-}
-
-const fetchSpecifiedDrivers = function(arr, fn) {
-  return fn(arr);
-}
+const fetchSpecifiedDrivers = function(arrayOfDrivers, whichDrivers) {
+    return whichDrivers(arrayOfDrivers);
+};
